9a54f877ce6e349f86c9db7805f8c850c34aacf9
kabziński.grzegorz <209343@edu.p.lodz.pl>
2019-03-19 18:42:31 +0100

    Isolated the way that you can count taxes

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Calculator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Calculator.java
@@ -1,5 +1,7 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-public interface Calculator {
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+public interface Calculator {
+    Tax calculate(RequestItem requestItem);
 }
