58332fe1a745d4e9fbea56ad1b514a8bc09a7c43
202323 <202323@CARBON.CTI.P.LODZ.PL>
2019-03-19 19:01:35 +0100

    New interface for various types of tax calculators

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculation.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.math.BigDecimal;
+
+public interface TaxCalculation {
+
+    public Tax calculate(RequestItem item);
+
+}
