f9f2f9d322b70d84296595a06531883f901f7e59
209420 <209420@edu.p.lodz.pl>
2019-03-19 18:24:56 +0100

    "Utworzony TaxCalculator"

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -22,39 +22,39 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class BookKeeper {
 
 
-    public Invoice issuance(InvoiceRequest invoiceRequest) {
+    public Invoice issuance(InvoiceRequest invoiceRequest,TaxClaculator taxClaculator) {
         Invoice invoice = Invoice.create(Id.generate(), invoiceRequest.getClient());
 
         for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
+//            BigDecimal ratio = null;
+//            String desc = null;
+//
+//            switch (item.getProductData()
+//                        .getType()) {
+//                case DRUG:
+//                    ratio = BigDecimal.valueOf(0.05);
+//                    desc = "5% (D)";
+//                    break;
+//                case FOOD:
+//                    ratio = BigDecimal.valueOf(0.07);
+//                    desc = "7% (F)";
+//                    break;
+//                case STANDARD:
+//                    ratio = BigDecimal.valueOf(0.23);
+//                    desc = "23%";
+//                    break;
+//
+//                default:
+//                    throw new IllegalArgumentException(item.getProductData()
+//                                                           .getType()
+//                                                       + " not handled");
+//            }
 
-            switch (item.getProductData()
-                        .getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
+//            Money taxValue = net.multiplyBy(ratio);
 
-                default:
-                    throw new IllegalArgumentException(item.getProductData()
-                                                           .getType()
-                                                       + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
-
+//            Tax tax = new Tax(taxValue, desc);
+            Tax tax = taxClaculator.calculate(item);
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
         }
