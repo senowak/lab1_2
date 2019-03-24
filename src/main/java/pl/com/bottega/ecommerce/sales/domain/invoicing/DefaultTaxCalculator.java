05f4cdb40056fa98c789d9ba71e67f50e4556afc
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-03-24 19:31:11 +0100

    implements interface TaxCalculator

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculator.java
@@ -4,7 +4,7 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import java.math.BigDecimal;
 
-public class DefaultTaxCalculator {
+public class DefaultTaxCalculator implements TaxCalculator{
     public Tax getTax(RequestItem item, Money net){
         BigDecimal ratio;
         String desc;
