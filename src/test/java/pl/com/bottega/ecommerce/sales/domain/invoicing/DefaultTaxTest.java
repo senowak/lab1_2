1fde211f39f2154308b40d79e58617ef8bb91c22
Paxon96 <bwloczyk@gmail.com>
2019-03-26 18:02:13 +0100

    add checkStandardTest in DefaultTax

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxTest.java
@@ -22,15 +22,19 @@ public class DefaultTaxTest {
 
     private final String DRUG_DESCRIPTION = "5% (D)";
     private final String FOOD_DESCRIPTION = "7% (F)";
+    private final String STANDARD_DESCRIPTION = "23%";
 
     private final BigDecimal VALUE_OF_DRUG = BigDecimal.valueOf(0.05);
     private final BigDecimal VALUE_OF_FOOD = BigDecimal.valueOf(0.07);
+    private final BigDecimal VALUE_OF_STANDARD = BigDecimal.valueOf(0.23);
 
-    @Before public void init() {
+    @Before
+    public void init() {
         defaultTax = new DefaultTax();
     }
 
-    @Test public void checkDrugTest() {
+    @Test
+    public void checkDrugTest() {
         productData = new ProductData(new Id("1"), new Money(1), "prod1", ProductType.DRUG, new Date());
         requestItem = new RequestItem(productData, 1, new Money(1));
 
@@ -40,11 +44,23 @@ public class DefaultTaxTest {
         Assert.assertThat(tax.getDescription(), is(equalTo(defaultTax.calculate(requestItem).getDescription())));
     }
 
-    @Test public void checkFoodTest() {
+    @Test
+    public void checkFoodTest() {
         productData = new ProductData(new Id("1"), new Money(1), "prod1", ProductType.FOOD, new Date());
         requestItem = new RequestItem(productData, 1, new Money(1));
 
         Tax tax = new Tax(requestItem.getTotalCost().multiplyBy(VALUE_OF_FOOD), FOOD_DESCRIPTION);
+
+        Assert.assertThat(tax.getAmount(), is(equalTo(defaultTax.calculate(requestItem).getAmount())));
+        Assert.assertThat(tax.getDescription(), is(equalTo(defaultTax.calculate(requestItem).getDescription())));
+    }
+
+    @Test
+    public void checkStandardTest() {
+        productData = new ProductData(new Id("1"), new Money(1), "prod1", ProductType.STANDARD, new Date());
+        requestItem = new RequestItem(productData, 1, new Money(1));
+
+        Tax tax = new Tax(requestItem.getTotalCost().multiplyBy(VALUE_OF_STANDARD), STANDARD_DESCRIPTION);
 
         Assert.assertThat(tax.getAmount(), is(equalTo(defaultTax.calculate(requestItem).getAmount())));
         Assert.assertThat(tax.getDescription(), is(equalTo(defaultTax.calculate(requestItem).getDescription())));
