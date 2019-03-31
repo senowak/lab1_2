20141a94de212fda516a0f1873f4a7f0102485d2
grzegorz kabzinski <grzesiek.kab@gmail.com>
2019-03-31 18:14:35 +0200

    DefaultTax refactor, and started doing MoneyTests

--- src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -1,10 +1,35 @@
 package pl.com.bottega.ecommerce.sharedkernel;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
 public class MoneyTest {
-    @Test
-    public void subtract() {
+    private Money dollarMoney;
+    private Money plnMoney;
+
+    @Before
+    public void initalizeMoney(){
+        dollarMoney = new Money(3.87,"USD");
+        plnMoney = new Money(1.00,"PLN");
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIfCurrenciesMatchForAddition(){
+        dollarMoney.add(plnMoney);
+    }
+
+    @Test
+    public void testCorrectAddition(){
+        Assert.assertThat(dollarMoney.add(dollarMoney),is(equalTo(new Money(2 * 3.87,"USD"))));
+    }
+
+
+
+
 }
