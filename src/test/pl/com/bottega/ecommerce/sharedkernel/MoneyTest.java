c149dc9016bb63bfc50c45ee404e2b70e70f02b9
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-04-01 20:09:12 +0200

    subtractMoneyInCompatibleCurrency test added

--- src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -11,10 +11,18 @@ import static org.hamcrest.Matchers.is;
 public class MoneyTest {
 
     @Test
-    public void addMoney() {
-        Money money = new Money(45.5, Currency.getInstance("USD"));
-        Money addend = new Money(55.8, Currency.getInstance("USD"));
+    public void addMoneyInCompatibleCurrency() {
+        Money money = new Money(55.8, Currency.getInstance("USD"));
+        Money addend = new Money(45.5, Currency.getInstance("USD"));
         Money sum = new Money((55.8 + 45.5), Currency.getInstance("USD"));
         Assert.assertThat(true, is(equalTo(money.add(addend).equals(sum))));
+    }
+
+    @Test
+    public void subtractMoneyInCompatibleCurrency() {
+        Money money = new Money(55.8, Currency.getInstance("USD"));
+        Money subtrahend = new Money(45.5, Currency.getInstance("USD"));
+        Money differenxe = new Money((55.8 - 45.5), Currency.getInstance("USD"));
+        Assert.assertThat(true, is(equalTo(money.subtract(subtrahend).equals(differenxe))));
     }
 }
