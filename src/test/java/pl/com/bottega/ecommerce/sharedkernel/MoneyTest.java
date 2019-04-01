a66ecf2aafd46cadd49c64c2e0f6002cd083f18f
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-04-01 22:53:48 +0200

    add test in MoneyTest Add SameCurrency

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -1,40 +1,21 @@
 package pl.com.bottega.ecommerce.sharedkernel;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Currency;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class MoneyTest {
 
     @Test
-    public void multiplyBy() {
+    public void AddSameCurrency() {
+        Money m1 = new Money(10, Currency.getInstance("UA"));
+        Money m2 = new Money(10, Currency.getInstance("UA"));
+        Money result = m1.add(m2);
+        Assert.assertThat(result.equals(new Money(20, Currency.getInstance("UA"))), is(true));
     }
 
-    @Test
-    public void multiplyBy1() {
-    }
-
-    @Test
-    public void add() {
-    }
-
-    @Test
-    public void subtract() {
-    }
-
-    @Test
-    public void getCurrencyCode() {
-    }
-
-    @Test
-    public void getCurrency() {
-    }
-
-    @Test
-    public void greaterThan() {
-    }
-
-    @Test
-    public void lessThan() {
-    }
 }
