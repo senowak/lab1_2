a4aab30ce190afde8d840e20dbe49790fd8fd7cc
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-04-01 22:59:32 +0200

    add test in MoneyTest AddSameCurrency fix

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -12,10 +12,11 @@ public class MoneyTest {
 
     @Test
     public void AddSameCurrency() {
-        Money m1 = new Money(10, Currency.getInstance("UA"));
-        Money m2 = new Money(10, Currency.getInstance("UA"));
+        Money m1 = new Money(5, Currency.getInstance("PLN"));
+        Money m2 = new Money(5, Currency.getInstance("PLN"));
         Money result = m1.add(m2);
-        Assert.assertThat(result.equals(new Money(20, Currency.getInstance("UA"))), is(true));
+        Assert.assertThat(result.equals(new Money(10, Currency.getInstance("PLN"))), is(true));
     }
+
 
 }
