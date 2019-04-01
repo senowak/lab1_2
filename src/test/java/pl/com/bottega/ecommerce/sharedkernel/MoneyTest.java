463c06ee2dbfcc71812cf73fd64d1f57bdd53e87
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-04-01 21:57:53 +0200

    test for Money added

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,29 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Currency;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    Currency currencyEUR;
+    Currency currencyUSD;
+
+    @Before
+    public void setUp() {
+        currencyEUR = Currency.getInstance("EUR");
+        currencyUSD = Currency.getInstance("USD");
+    }
+
+    @Test
+    public void AddMoneyWithSameCurrency() {
+        Money m1 = new Money(10, currencyEUR);
+        Money m2 = new Money(10, currencyEUR);
+        Money result = m1.add(m2);
+        Assert.assertThat(result.equals(new Money(20, currencyEUR)), is(true));
+    }
