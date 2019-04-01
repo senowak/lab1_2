d7fb6bcd023c0571ef5efdd2bd9411fa3eee6b1d
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-04-01 20:04:10 +0200

    addMoney test added

---
+++ src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,20 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Currency;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class MoneyTest {
+
+    @Test
+    public void addMoney() {
+        Money money = new Money(45.5, Currency.getInstance("USD"));
+        Money addend = new Money(55.8, Currency.getInstance("USD"));
+        Money sum = new Money((55.8 + 45.5), Currency.getInstance("USD"));
+        Assert.assertThat(true, is(equalTo(money.add(addend).equals(sum))));
+    }
+}
