b330449d23f431fba991082b7410c06182093cec
Sebastian Nowak <neko10a@gmail.com>
2019-04-15 13:11:38 +0200

    Refaktoryzacja BookKeeper pod względem podatku

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -12,45 +12,18 @@
  */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import java.math.BigDecimal;
-
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(InvoiceRequest invoiceReq) {
+    public Invoice issuance(InvoiceRequest invoiceReq, TaxCalculation taxCalculation) {
         Invoice invoice = Invoice.create(Id.generate(), invoiceReq.getClient());
 
         for (RequestItem item : invoiceReq.getItems()) {
             Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
 
-            switch (item.getProductData()
-                        .getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
-
-                default:
-                    throw new IllegalArgumentException(item.getProductData()
-                                                           .getType()
-                                                       + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
+            Tax tax = taxCalculation.calculate(item, net);
 
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -0,0 +1,40 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import java.math.BigDecimal;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class TaxCalculator implements TaxCalculation {
+
+    BigDecimal ratio;
+    String desc;
+
+    @Override
+    public Tax calculate(RequestItem item, Money net) {
+        switch (item.getProductData()
+                    .getType()) {
+            case DRUG:
+                ratio = BigDecimal.valueOf(0.05);
+                desc = "5% (D)";
+                break;
+            case FOOD:
+                ratio = BigDecimal.valueOf(0.07);
+                desc = "7% (F)";
+                break;
+            case STANDARD:
+                ratio = BigDecimal.valueOf(0.23);
+                desc = "23%";
+                break;
+
+            default:
+                throw new IllegalArgumentException(item.getProductData()
+                                                       .getType()
+                                                   + " not handled");
+        }
+
+        Money taxValue = net.multiplyBy(ratio);
+
+        return new Tax(taxValue, desc);
+    }
+
+}
